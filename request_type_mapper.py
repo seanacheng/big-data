a5b0f7ae92793d1a708630b97ef8d777cb924bf0
Sean Cheng <seanacheng@gmail.com>
2024-10-12 21:36:27 -0400

    add try catch in for loop

--- request_type_mapper.py
+++ request_type_mapper.py
@@ -10,8 +10,11 @@ def main(argv):
     try:
         while line:
             for match in pattern.findall(line):
-                rq_type = match[1]
-                print ('LongValueSum:'+str(rq_type)+'\t'+'1')
+                try:
+                    rq_type = match[1]
+                    print ('LongValueSum:'+str(rq_type)+'\t'+'1')
+                except:
+                    continue
 
             line = sys.stdin.readline()
     except EOFError as error:
